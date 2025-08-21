dc0005676de3abc501d1fc334475a2c31381eeea
AnthiaXTech <carly@anthiax.io>
2025-08-21 22:55:17 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (_, res) => res.send('Spotify bot is live and on repeat!'));
+app.get('/', (_, res) => res.send('Spotify bot is live and on repeat!💥'));
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log('Listening on', PORT));
